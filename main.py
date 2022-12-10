e57bcfdf70ac848e39c34e41c7c30eeb74dc2eaf
Arrtyoom <samuel.gross67@gmail.com>
2022-12-10 18:32:33 +0100

    finish of Linear Regression p.1

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ from sklearn.utils import shuffle
 
 data = pd.read_csv("student-mat.csv", sep=";")
 
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+predict = "G3"
+
+x = np.array(data.drop([predict], 1))  # attribute
+y = np.array(data[predict])  # labels
+
+# x_train and y train will be x and y split up
+# x_test and y_test will be what test de accuracy of our model
+x_train, y_train, x_test, y_train = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+
