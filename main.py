32e4672354cfa294bc48cb9649e752acde58cfd4
Arrtyoom <samuel.gross67@gmail.com>
2022-12-10 18:34:09 +0100

    finish of Linear Regression p.1

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ x = np.array(data.drop([predict], 1))  # attribute
 y = np.array(data[predict])  # labels
 
 # x_train and y train will be x and y split up
-# x_test and y_test will be what test de accuracy of our model
-x_train, y_train, x_test, y_train = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+# x_test and y_test will be what test de accuracy of our model (x and y)
+# test_size="0.1" will take 10% of our model value
+x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
 
